213895a11a9af720a9fd3ee5d0a5c87715a480f7
UlianaGritchina <ugritchina@icloud.com>
2022-02-16 15:58:40 +0300

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Ульяна Гритчина on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
